62e81381984a90822429aa9e89327b77762ab43f
Lucinda <luxinxuan0312@outlook.com>
2022-07-12 16:58:16 -0400

    daily update 0712

--- dataDownload.py
+++ dataDownload.py
@@ -15,6 +15,23 @@ import jieba.analyse
 import poplib
 
 #%%code cell
+#set config
 with open ("./config.json",'r',encoding='utf-8-sig') as f:
-	d= json.load(f)
+	params= json.load(f)
+# %%code cell
+
+poplib._MAXLINE=20480
+server='outlook.office365.com'
+port=995
+user='syslucinda@outlook.com'
+pswd='200312Az'
+pop=poplib.POP3_SSL(server,port)
+pop.user(user)
+pop.pass_(pswd)
+# %%code cell
+poplist=pop.list()
+numMessage = len(poplist[1])
+popuid=pop.uidl()
+
+
 # %%
